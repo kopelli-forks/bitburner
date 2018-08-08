457ff862d36d4bce1a6b1251c5d67b54b3d7955a
Steven Evans <kopelli@gmail.com>
2018-08-07 21:41:44 -0400

    Fixing the Bash syntax"

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -ev
-if [ "${TRAVIS}" = "true" && "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    if [ "${TRAVIS_BRANCH}" = "master" || "${TRAVIS_BRANCH}" = "dev" ]; then
+if [ "${TRAVIS}" = "true" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    if [ "${TRAVIS_BRANCH}" = "master" ] || [ "${TRAVIS_BRANCH}" = "dev" ]; then
         git checkout ${TRAVIS_BRANCH}
     fi
 fi
 
 npm run build
 
-if [ "${TRAVIS}" = "true" && "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [ "${TRAVIS}" = "true" ] &&  [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     git add -u
     git commit -m "Updating bundles from ${TRAVIS_JOB_NUMBER}"
     git push
